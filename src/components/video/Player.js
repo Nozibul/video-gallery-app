811ed6461f388813613bfa6f4c58fe91af9db19e
Nozibul <nozibulislamspi@gmail,com>
2023-01-26 20:13:23 +0600

    store configartion and api query

--- src/components/video/Player.js
+++ src/components/video/Player.js
@@ -5,7 +5,7 @@ export default function Player({link, title}) {
             className="aspect-video"
             src={link}
             title={title}
-            frameBorder=""
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
         ></iframe>
